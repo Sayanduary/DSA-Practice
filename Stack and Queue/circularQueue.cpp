110819ecc68bac39c213464c18ef5cb8e128b1da
Sayan Duary <sayanduary@gmail.com>
2024-11-18 19:29:02 +0530

    Queue Operation Done

--- Stack and Queue/circularQueue.cpp
+++ Stack and Queue/circularQueue.cpp
@@ -1,70 +1,74 @@
 #include <iostream>
 using namespace std;
-class Queue
+struct Queue
 {
-private:
+  int size;
   int front;
   int rear;
-  int size;
-  int *arr;
-
-public:
-  Queue()
-  {
-    front = rear = 0;
-    this->size = 10;
-    arr = new int[this->size];
-  }
-  Queue(int size)
-  {
-    front = rear = 0;
-    this->size = size;
-    arr = new int[this->size];
-  }
-  void enQueue(int x);
-  int deQueue();
-  void display();
-};
-void Queue::enQueue(int x)
+  int *array;
+} *q;
+Queue *create(int size)
 {
-  if ((rear + 1) % this->size == front)
+  Queue *q = (Queue *)malloc(sizeof(Queue));
+  if (q == NULL)
   {
-    cout << "queue is full";
+    return NULL; // Handle memory allocation failure
   }
-  rear = (rear + 1) % size;
-  arr[rear] = x;
-}
-int Queue::deQueue()
-{
-  int x = -1;
-  if (rear == front)
+
+  q->size = size;
+  q->front = q->rear = -1;
+  q->array = (int *)malloc(q->size * sizeof(int));
+  if (q->array == NULL)
   {
-    cout << "Queue is Empty ";
-    return -1;
+    free(q); // Free allocated memory if array allocation fails
+    return NULL;
   }
-  front = (front + 1) % size;
-  x = arr[front];
 
-  return x;
+  return q; // Return the created queue
 }
-
-void Queue::display()
+int isEmpty(struct Queue *q)
 {
-  int i = front + 1;
-  do
-  {
-    cout << arr[i] << " ";
-    i = (i + 1) % size;
-  } while (i != (rear + 1) % size);
+  return q->front == q->rear;
 }
-
+int isFull(Queue *q)
+{
+  return (q->rear + 1) % q->size == q->front;
+}
+void enQueue(struct Queue *q, int val)
+{
+  if (isFull(q))
+  {
+    cout << "Queue is Overflowed ";
+  }
+  q->rear = (q->rear + 1) % q->size;
+  q->array[q->rear] = val;
+}
+int deQueue(Queue *q)
+{
+  int val = -1;
+  if (isEmpty(q))
+  {
+    cout << "Queue is Underflowed ";
+    return val;
+  }
+  q->front = (q->front + 1) % q->size;
+  val = q->front;
+  return val;
+}
+void display(Queue *q)
+{
+  int i;
+  for (i = (q->front + 1) % q->size; i <= q->rear; i++)
+  {
+    cout << q->array[i] << " ";
+  }
+}
 int main()
 {
-  Queue q;
-  q.enQueue(20);
-  q.enQueue(40);
-  q.enQueue(80);
-  q.deQueue();
-  q.display();
+  Queue *q = create(10);
+  enQueue(q, 10);
+  enQueue(q, 20);
+  deQueue(q);
+  display(q);
   return 0;
 }
