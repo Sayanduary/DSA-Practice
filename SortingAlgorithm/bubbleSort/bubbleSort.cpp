13a3a1f49a4c61110c332efc688812789289267c
Sayan Duary <sayanduary@gmail.com>
2024-11-20 09:21:19 +0530

    Done

--- SortingAlgorithm/bubbleSort/bubbleSort.cpp
+++ SortingAlgorithm/bubbleSort/bubbleSort.cpp
@@ -11,8 +11,11 @@ void printArray(int *A, int n)
 }
 void bubbleSort(int *A, int n)
 {
+  int isSorted = 0;
   for (int i = 0; i < n - 1; i++) // for number of passes
   {
+    cout << "Working on pass number " << i<<endl;
+    isSorted = 1;
     for (int j = 0; j < n - 1 - i; j++) // number of comparisions
     {
       if (A[j] > A[j + 1])
@@ -20,7 +23,12 @@ void bubbleSort(int *A, int n)
         int temp = A[j];
         A[j] = A[j + 1];
         A[j + 1] = temp;
+        isSorted = 0;
       }
+    }
+    if (isSorted)
+    {
+      return;
     }
   }
 }
@@ -28,12 +36,13 @@ void bubbleSort(int *A, int n)
 int main()
 {
   int A[] = {12, 23, 10, 11, 60};
+  int B[] = {10, 12, 13, 14, 15, 16};
   int length = sizeof(A) / sizeof(A[0]);
   cout << "Before Sorting Array: ";
-  printArray(A, length);
-  bubbleSort(A, length);
+  printArray(B, length);
+  bubbleSort(B, length);
 
   cout << "Sorted Arary : ";
-  printArray(A, length);
+  printArray(B, length);
   return 0;
 }
