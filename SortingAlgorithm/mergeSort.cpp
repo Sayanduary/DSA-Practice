d3e0ba9d3c7bef051f2bbc8fb080beab4b926f57
Sayan Duary <sayanduary@gmail.com>
2024-11-21 10:48:23 +0530

    done

---
+++ SortingAlgorithm/mergeSort.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+void printArray(int *arr, int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
+}
+void merge(int *arr, int left, int mid, int right)
+{
+  int n1 = mid - left + 1;
+  int n2 = (right - mid);
+  int L[n1], R[n2];
+  for (int i = 0; i < n1; i++)
+  {
+    L[i] = arr[left + i];
+  }
+  for (int j = 0; j < n2; j++)
+  {
+    R[j] = arr[mid + 1 + j];
+  }
+  int i = 0, j = 0, k = left;
+  while (i < n1 && j < n2)
+  {
+    if (L[i] <= R[j])
+    {
+      arr[k] = L[i];
+      i++;
+    }
+    else
+    {
+      arr[k] = R[j];
+      j++;
+    }
+    k++;
+  }
+  for (; i < n1; i++)
+  {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+  for (; j < n2; j++)
+  {
+    arr[k] = R[j];
+    j++;
+    k++;
+  }
+}
+
+void mergeSort(int *arr, int left, int right)
+{
+  if (left < right)
+  {
+    int mid = left + (right - left) / 2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
+  }
+}
+int main()
+{
+  int arr[] = {12, 11, 13, 5, 6, 7};
+  int length = sizeof(arr) / sizeof(arr[0]);
+  printArray(arr, length);
+  cout << endl;
+  mergeSort(arr, 0, length - 1);
+  printArray(arr, length);
+
+  return 0;
+}
