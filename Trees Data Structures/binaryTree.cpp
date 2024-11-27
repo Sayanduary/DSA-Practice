05475d5d80cedb653d8a16b2596ded2f345e6f71
Sayan Duary <sayanduary@gmail.com>
2024-11-27 11:19:17 +0530

    done

--- Trees Data Structures/binaryTree.cpp
+++ Trees Data Structures/binaryTree.cpp
@@ -5,6 +5,12 @@ struct Node
   int data;
   Node *left;
   Node *right;
+
+  Node(int val)
+  {
+    data = val;
+    left = right = nullptr;
+  }
 };
 void preOrder(Node *root)
 {
@@ -25,6 +31,15 @@ void postOdrder(Node *root)
     cout << root->data;
   }
 }
+void inOrder(Node *root)
+{
+  if (root != NULL)
+  {
+    inOrder(root->left);
+    cout << root->data;
+    inOrder(root->right);
+  }
+}
 Node *createNode(int data)
 {
   Node *rootNode = (Node *)malloc(sizeof(Node));
@@ -33,20 +48,41 @@ Node *createNode(int data)
   rootNode->right = NULL;
   return rootNode;
 }
+int isBST(Node *root)
+{
+  if (root != NULL)
+  {
+    isBST(root->left);
+    cout << " " << root->data;
+    isBST(root->right);
+  }
+}
 int main()
 {
-  Node *rootNode = createNode(4);
-  Node *p = createNode(1);
-  Node *p1 = createNode(5);
-  Node *p2 = createNode(2);
-  Node *p3 = createNode(6);
+  Node *rootNode = createNode(9);
+  Node *p = createNode(4);
+  Node *p1 = createNode(2);
+  Node *p2 = createNode(7);
+  Node *p3 = createNode(5);
+  Node *p4 = createNode(8);
+  Node *p5 = createNode(11);
+  Node *p6 = createNode(15);
+  Node *p7 = createNode(14);
   rootNode->left = p;
-  rootNode->right = p3;
+  rootNode->right = p5;
   p->left = p1;
   p->right = p2;
-  preOrder(rootNode);
-  cout << endl;
-  postOdrder(rootNode);
-  cout << endl;
+  p2->left = p3;
+  p2->right = p4;
+  p5->right = p6;
+  p6->left = p7;
+  // preOrder(rootNode);
+  // cout << endl;
+  // postOdrder(rootNode);
+  // cout << endl;
+  // inOrder(rootNode);
+
+  isBST(rootNode);
+
   return 0;
 }
