6740a141898857dc19cf81a6fbc7a1bf0dcd6e46
Sayan Duary <sayanduary@gmail.com>
2024-12-18 04:47:12 +0000

    done

--- Queue/circularQueue.cpp
+++ Queue/circularQueue.cpp
@@ -1,65 +1,64 @@
 #include <iostream>
 using namespace std;
-struct Queue
-{
-  int *arr;
-  int front;
-  int rear;
-  int size;
+
+struct Queue {
+    int *arr;
+    int front;
+    int rear;
+    int size;
 };
-void create(Queue *q, int size)
-{
-  q->size = size;
-  q->front = q->rear = 0;
-  q->arr = (int *)malloc(q->size * sizeof(int));
-}
-void enQueue(struct Queue *q, int data)
-{
-  if ((q->rear + 1) % q->size == q->front)
-  {
-    cout << "Queue is Full";
-    return;
-  }
-  q->rear = ((q->rear + 1) % q->size);
-  q->arr[q->rear] = data;
+
+void create(Queue &q, int size) {
+    q.size = size;
+    q.front = q.rear = 0;
+    q.arr = new int[q.size];
 }
 
-int deQueue(Queue *q)
-{
-
-  if (q->front == q->rear)
-  {
-    cout << "Queue is Empty";
-    return -1;
-  }
-  q->front = ((q->front + 1) % q->size);
-  return q->arr[q->front];
+void enQueue(Queue &q, int data) {
+    if ((q.rear + 1) % q.size == q.front) {
+        cout << "Queue is Full" << endl;
+        return;
+    }
+    q.rear = (q.rear + 1) % q.size;
+    q.arr[q.rear] = data;
 }
 
-void display(Queue *q)
-{
-  if (q->front == q->rear)
-  {
-    cout << "Queue is Empty";
-    return;
-  }
-  int i = (q->front + 1) % q->size;
-  while (i != (q->rear + 1) % q->size)
-  {
-    cout << i << q->arr[i] << " ";
-    i = (i + 1) % q->size;
-  }
+int deQueue(Queue &q) {
+    if (q.front == q.rear) {
+        cout << "Queue is Empty" << endl;
+        return -1;
+    }
+    q.front = (q.front + 1) % q.size;
+    return q.arr[q.front];
 }
-int main()
-{
-  struct Queue *q;
-  create(q, 5);
-  enQueue(q, 10);
-  enQueue(q, 13);
-  enQueue(q, 13);
-  deQueue(q);
-  deQueue(q);
-  display(q);
 
-  return 0;
+void display(Queue &q) {
+    if (q.front == q.rear) {
+        cout << "Queue is Empty" << endl;
+        return;
+    }
+    int i = (q.front + 1) % q.size;
+    while (i != (q.rear + 1) % q.size) {
+        cout << q.arr[i] << " ";
+        i = (i + 1) % q.size;
+    }
+    cout << endl;
+}
+
+int main() {
+    Queue q;
+    create(q, 5);
+
+    enQueue(q, 10);
+    enQueue(q, 13);
+    enQueue(q, 15);
+
+    cout << "Dequeued: " << deQueue(q) << endl;
+    cout << "Dequeued: " << deQueue(q) << endl;
+
+    cout << "Queue Contents: ";
+    display(q);
+
+    delete[] q.arr; // Free allocated memory
+    return 0;
 }
